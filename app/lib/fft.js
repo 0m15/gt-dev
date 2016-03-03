a7db89d54b85d0a1046c9cf7ecba1658f0ca1639
Simone Carella <simo@Simones-MacBook-Pro.local>
2016-03-03 13:44:09 +0100

    build

--- app/lib/fft.js
+++ app/lib/fft.js
@@ -1,33 +1,10 @@
+function getAudioContext() {
+  return (window.AudioContext||window.webkitAudioContext)
+}
+
 export default function fft(id) {
 
   var audio = document.getElementById('track')
-  var audioContext = new AudioContext()
-  var source = audioContext.createMediaElementSource(audio)
-  var analyser = audioContext.createAnalyser()
-  
-  source.connect(analyser);
-  analyser.connect(audioContext.destination)
 
-  analyser.fftSize = 256
-
-
-  // var bufferLength = analyser.frequencyBinCount
-  // var dataArray = new Uint8Array(analyser.frequencyBinCount)
-  
- 
-  // frequencyBinCount tells you how many values you'll receive from the analyser
-  //var frequencyData = new Uint8Array(analyser.frequencyBinCount)
- 
-
-  // function renderFrame() {
-  //    requestAnimationFrame(renderFrame);
-  //    // update data in frequencyData
-  //    analyser.getByteFrequencyData(frequencyData);
-  //    // render frame based on values in frequencyData
-  // }
-
-  
-  //audio.play()
-
-  return { analyser, audio }
+  return { audio }
 }
--- assets/app.js
+++ assets/app.js
@@ -62204,34 +62204,15 @@
 	  value: true
 	});
 	exports.default = fft;
+	function getAudioContext() {
+	  return window.AudioContext || window.webkitAudioContext;
+	}
+
 	function fft(id) {
 
 	  var audio = document.getElementById('track');
-	  var audioContext = new AudioContext();
-	  var source = audioContext.createMediaElementSource(audio);
-	  var analyser = audioContext.createAnalyser();
 
-	  source.connect(analyser);
-	  analyser.connect(audioContext.destination);
-
-	  analyser.fftSize = 256;
-
-	  // var bufferLength = analyser.frequencyBinCount
-	  // var dataArray = new Uint8Array(analyser.frequencyBinCount)
-
-	  // frequencyBinCount tells you how many values you'll receive from the analyser
-	  //var frequencyData = new Uint8Array(analyser.frequencyBinCount)
-
-	  // function renderFrame() {
-	  //    requestAnimationFrame(renderFrame);
-	  //    // update data in frequencyData
-	  //    analyser.getByteFrequencyData(frequencyData);
-	  //    // render frame based on values in frequencyData
-	  // }
-
-	  //audio.play()
-
-	  return { analyser: analyser, audio: audio };
+	  return { audio: audio };
 	}
 
 /***/ },
